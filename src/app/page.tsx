208c5ddd814b91a57c0389b4da2df45113ed0b33
Miran <mirankavinda12@gmail.com>
2024-03-30 06:10:22 +0530

    Add button component and update navbar styling

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import Navbar from "@/components/global/navbar";
 import { ContainerScroll } from "@/components/global/container-scroll-animation";
+import { Button } from '@/components/ui/button'
 import Image from "next/image";
 
 export default function Home() {
@@ -16,10 +17,20 @@ export default function Home() {
         <div className="flex flex-col mt-[-100px] md:mt-[-50px]">
           <ContainerScroll
             titleComponent={
-              <div className="flex items-center justify-center
-              flex-col"></div>
+              <div className="flex items-center justify-center flex-col">
+                <Button size={'lg'}
+                className="p-8 mb-8 md:mb-0 text-2xl w-full sm:w-fit border-t-2 rounded-full border-[#4D4D4D] bg-[#1F1F1F] hover:bg-white group transition-all flex items-center justify-center gap-4 hover:shadow-xl hover:shadow-neutral-500 duration-500"
+                >
+                  <span className="bg-clip-text text-transparent bg-gradient-to-r from-neutral-500 to-neutral-600  md:text-center font-sans group-hover:bg-gradient-to-r group-hover:from-black goup-hover:to-black">
+                    Start For Free Today
+                  </span>
+                </Button>
+                <h1 className="text-5xl md:text-8xl  bg-clip-text text-transparent bg-gradient-to-b from-white to-neutral-600 font-sans font-bold">
+                  Automate Your Work With LinkLeap
+                </h1>
+              </div>
             }
-          ></ContainerScroll>
+          />
         </div>
     </section>
     </main>
--- src/components/global/navbar.tsx
+++ src/components/global/navbar.tsx
@@ -22,7 +22,7 @@ const Navbar = async (props: Props) => {
         </aside>
         <nav className="absolute left-[50%] top-[50%] transform translate-x-[-50%] translate-y-[-50%]
         hidden md:block">
-            <ul className="flex items-center gap-4 list-none">
+            <ul className="flex items-center gap-6 list-none">
             <li>
                 <Link href="#">Products</Link>
             </li>
