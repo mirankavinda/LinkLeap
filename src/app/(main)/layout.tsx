afefe7fa25625d282f4e031494bfe1d3eb9c02b6
Miran <mirankavinda12@gmail.com>
2024-04-02 12:04:59 +0530

    fixed typo

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -2,11 +2,11 @@ import React from 'react'
 
 type Props = { children: React.ReactNode }
 
-const Layout = ({ children }: Props) => {
+const Layout = (props : Props) => {
   return (
-    <div className="border-l-[1px] border-t-[1px] pb-20 h-screen rounded-l-3xl 
-    border-muted-foreground/20 overflow-scroll ">
-      {children}
+    <div className="flex overflow-hidden h-screen">
+      <div className="w-full">{props.
+        children}</div>
     </div>
   )
 }
