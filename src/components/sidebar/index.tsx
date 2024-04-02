bb66ddd19ce0933c8bd71fa6681cb9d4efc96825
Miran <mirankavinda12@gmail.com>
2024-04-02 16:23:00 +0530

    Add @radix-ui/react-tooltip dependency and update sidebar component

--- src/components/sidebar/index.tsx
+++ src/components/sidebar/index.tsx
@@ -1,6 +1,14 @@
 'use client'
+import Link from 'next/link'
 import { usePathname } from 'next/navigation'
 import React from 'react'
+import {
+    Tooltip,
+    TooltipContent,
+    TooltipProvider,
+    TooltipTrigger,
+  } from "@/components/ui/tooltip"
+
 
 type Props = {}
 
@@ -8,7 +16,21 @@ const MenuOptions = (props: Props) => {
     const pathName = usePathname()
 
     return <nav className=" dark:bg-black h-screen overflow-scroll justify-between
-    flex items-center flex-col"></nav>
+    flex items-center flex-col gap-10 py-6 px-2">
+        <div className="flex items-center justify-center flex-col gap-8">
+            <Link
+                className="flex font-bold flex-row"
+                href="/"
+            >
+                linkLeap.
+            </Link>
+            <TooltipProvider>
+                {menuOptions.map(() => {
+                    <div></div>
+                })}
+            </TooltipProvider>
+        </div>
+    </nav>
 }
 
 export default MenuOptions
