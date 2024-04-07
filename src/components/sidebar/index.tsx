425df2ebce24db0855fdaabb075e8e5f2aa92022
Miran <mirankavinda12@gmail.com>
2024-04-07 16:29:00 +0530

    Add @radix-ui/react-dropdown-menu dependency and update package.json and sidebar component

--- src/components/sidebar/index.tsx
+++ src/components/sidebar/index.tsx
@@ -12,6 +12,7 @@ import { menuOptions } from '@/lib/constant'
 import clsx from 'clsx'
 import { Separator } from '@/components/ui/separator'
 import { Database, GitBranch, LucideMousePointerClick } from 'lucide-react'
+import { ModeToggle } from '../global/mode-toggle'
 
 
 type Props = {}
@@ -108,7 +109,12 @@ const MenuOptions = (props: Props) => {
                         [-50%] -bottom-[30px]"/>
                     </div>
                 </div>
-        </div>
+            </div>
+            {/* Remove Darkmode | Light Mode */}
+            {/* <div className="flex items-center justify-center flex-col 
+            gap-8">
+                <ModeToggle/>
+            </div> */}
         </nav>
     )
 }
