62a9542f3b250007c9583d0fca4b2f67a0f4b725
Miran <mirankavinda12@gmail.com>
2024-04-07 14:12:36 +0530

    Add mouse click and git branch icons to sidebar menu options

--- src/components/sidebar/index.tsx
+++ src/components/sidebar/index.tsx
@@ -11,6 +11,7 @@ import {
 import { menuOptions } from '@/lib/constant'
 import clsx from 'clsx'
 import { Separator } from '@/components/ui/separator'
+import { GitBranch, LucideMousePointerClick } from 'lucide-react'
 
 
 type Props = {}
@@ -59,6 +60,32 @@ const MenuOptions = (props: Props) => {
                 ))}
                 </TooltipProvider>
                 <Separator />
+                <div className="flex items-center flex-col gap-9
+                dark:bg:-[#353346]/30 py-4 px-2 rounded-full h-56
+                overflow-scroll border-[1px]">
+                    <div className="relative dark:bg-[#353346]/70 p-2
+                    rounded-full dark:border-t-[2px] border-[1px]
+                    dark:border-t-[#353346]">
+                        <LucideMousePointerClick
+                            className="dark:text-white"
+                            size={18}
+                        />
+                        <div className="border-l-2 border-muted-foreground/50 
+                        h-6 absolute left-1/2 transform translate-x-
+                        [-50%] -bottom-[30px]"/>
+                    </div>
+                    <div className="relative dark:bg-[#353346]/70 p-2
+                    rounded-full dark:border-t-[2px] border-[1px]
+                    dark:border-t-[#353346]">
+                        <GitBranch
+                            className="text-muted-foreground"
+                        size={18}    
+                        />
+                        <div className="border-l-2 border-muted-foreground/50 
+                        h-6 absolute left-1/2 transform translate-x-
+                        [-50%] -bottom-[30px]"/>
+                    </div>
+                </div>
         </div>
         </nav>
     )
